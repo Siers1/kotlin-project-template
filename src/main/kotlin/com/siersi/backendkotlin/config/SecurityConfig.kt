e08dfcea5239388809f3f07c095c3c4e313d4be6
Siersi <siersione@foxmail.com>
2026-05-07 19:44:38 +0800

    add CorsConfiguration

--- src/main/kotlin/com/siersi/backendkotlin/config/SecurityConfig.kt
+++ src/main/kotlin/com/siersi/backendkotlin/config/SecurityConfig.kt
@@ -4,8 +4,6 @@ import com.siersi.backendkotlin.config.jwt.JwtEntryPoint
 import com.siersi.backendkotlin.config.jwt.JwtFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -13,6 +11,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +26,7 @@ class SecurityConfig(
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .csrf { it.disable() }
+            .cors { it.configurationSource(this.corsConfigurationSource()) }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .exceptionHandling { it.authenticationEntryPoint(jwtEntryPoint) }
             .authorizeHttpRequests { auth ->
@@ -38,10 +40,19 @@ class SecurityConfig(
         return http.build()
     }
 
-//    @Bean
-//    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
-//        return authenticationConfiguration.authenticationManager
-//    }
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val config = CorsConfiguration().apply {
+            allowedOriginPatterns = listOf("*")
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("*")
+            maxAge = 3600L
+        }
+
+        return UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", config)
+        }
+    }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
